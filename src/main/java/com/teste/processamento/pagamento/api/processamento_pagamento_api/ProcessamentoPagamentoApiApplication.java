8b29d58ea7c9eb07317e3bb2c4006e343b779991
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-11-29 00:28:20 -0300

    first commit

---
+++ src/main/java/com/teste/processamento/pagamento/api/processamento_pagamento_api/ProcessamentoPagamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.teste.processamento.pagamento.api.processamento_pagamento_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoPagamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoPagamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/processamento/pagamento/api/processamento_pagamento_api/ProcessamentoPagamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.processamento.pagamento.api.processamento_pagamento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoPagamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
